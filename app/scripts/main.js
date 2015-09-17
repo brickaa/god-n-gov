3083e505e0d9d2fd90b6c0b6794833b094470e20
becca <brickaa@users.noreply.github.com>
2015-09-17 12:42:04 -0500

    scrub custom video, add example code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,12 +3,6 @@
 var $main = $('#main'),
     $menuBtn = $('#menu-chapters-btn'),
     $menuChapters = $('#menu-chapters'),
-    $video = $('#video'),
-    $videoControls = $('#video-controls'),
-    video = $video[0],
-    $fullScreenBtn = $('#full-screen'),
-    $playBtn = $('#play-pause'),
-    seekBar = document.getElementById('seek-bar'),
     $startBtn = $('#start-video'),
     $videoCover = $('#video-cover-wrapper');
 
@@ -17,37 +11,9 @@ function menuAccordion(e) {
   $menuChapters.slideToggle(250);
 }
 
-function playPause(e) {
-  e.preventDefault();
-  if (video.paused === true) {
-    video.play();
-    $('.icon-TT-god_play').hide();
-    $('.icon-TT-god_pause').show();
-  } else {
-    video.pause();
-    $('.icon-TT-god_pause').hide();
-    $('.icon-TT-god_play').show();
-  }
-}
-
-function fullScreen(e) {
-  e.preventDefault();
-
-  if (video.requestFullscreen) {
-    video.requestFullscreen();
-  } else if (video.mozRequestFullScreen) {
-    video.mozRequestFullScreen(); // Firefox
-  } else if (video.webkitRequestFullscreen) {
-    video.webkitRequestFullscreen(); // Chrome and Safari
-  }
-}
-
 function startVideo(e) {
   e.preventDefault();
   $videoCover.hide();
-  $video.show();
-  $videoControls.show();
-  playPause(e);
 }
 
 function resize() {
@@ -60,50 +26,11 @@ $(document).ready(function() {
 
   $startBtn.click(startVideo);
   $menuBtn.click(menuAccordion);
-  $playBtn.click(playPause);
-  $fullScreenBtn.click(fullScreen);
 
   $main.click(function() {
     if($('#menu-chapters').is(':visible')) {
       $('#menu-chapters').slideToggle(250);
     }
-  });
-
-  // Event listener for the seek bar
-  seekBar.addEventListener('change', function() {
-    // Calculate the new time
-    var time = video.duration * (seekBar.value / 100);
-
-    // Update the video time
-    video.currentTime = time;
-  });
-
-  // Update the seek bar as the video plays
-  video.addEventListener('timeupdate', function() {
-    // Calculate the slider value
-    var value = (100 / video.duration) * video.currentTime;
-
-    // Update the slider value
-    seekBar.value = value;
-  });
-
-  // Pause the video when the slider handle is being dragged
-  seekBar.addEventListener('mousedown', function() {
-    video.pause();
-    $('.icon-TT-god_pause').hide();
-    $('.icon-TT-god_play').show();
-  });
-
-  // Play the video when the slider handle is dropped
-  seekBar.addEventListener('mouseup', function() {
-    video.play();
-    $('.icon-TT-god_play').hide();
-    $('.icon-TT-god_pause').show();
-  });
-
-  video.addEventListener('ended', function() {
-    $('.icon-TT-god_pause').hide();
-    $('.icon-TT-god_play').show();
   });
 
 });
